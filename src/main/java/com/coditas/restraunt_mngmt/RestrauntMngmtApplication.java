4eeef88eb61388a9ba502f6752451a20ca3dbb0e
Abideen Kasu <abideen.kasu@coditas.com>
2026-05-20 21:16:01 +0530

    initial

---
+++ src/main/java/com/coditas/restraunt_mngmt/RestrauntMngmtApplication.java
@@ -0,0 +1,14 @@
+package com.coditas.restraunt_mngmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrauntMngmtApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestrauntMngmtApplication.class, args);
+    }
+
+}
